839b1ff39bebcfc01ec507cc678be2809cee33cd
Drew Sommer <drew@sommerengineering.com>
2024-10-04 15:22:41 +0300

    feat: Create command line progress bar

--- stub.py
+++ stub.py
@@ -1,9 +1,35 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 class Engine():
-    def __init__(self):
+
+    # initialize
+    def __init__(self, initial_cash=100000):
+
+        # private var
+        self.cash = initial_cash
+        self.data = None
+        self.strategy = None
+        self.current_idx = None
+
+    # setters
+    def add_data(self, data: pd.DataFrame):
+        self.data = data
+    def add_strategy(self, strategy):
+        self.strategy = strategy
+
+    def run(self):
+        self.strategy.data = self.data
+
+        # tqdm is command line progress bar
+        for idx in tqdm(self.data.index):
+            self.current_idx = idx
+            self.strategy.current_idx = self.current_idx
+            self._fill_orders()
+            # self.strategy.on_bar()
+            print(idx)
+
+    def _fill_orders(self):
         pass
 
 class Strategy():
