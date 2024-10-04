28a146b08bd269c690ecf6d2cadc1106304eaedc
Drew Sommer <drew@sommerengineering.com>
2024-10-04 15:06:34 +0300

    feat: Stub project structure

---
+++ stub.py
@@ -0,0 +1,15 @@
+class Engine():
+    def __init__(self):
+        pass
+
+class Strategy():
+    def __init__(self):
+        pass
+
+class Trade():
+     def __init__(self):
+        pass
+
+class Order():
+    def __init__(self):
+        pass
