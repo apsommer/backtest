b6e379ebc659a60c50c3e16f1446c5be08d6b5c1
Drew Sommer <drew@sommerengineering.com>
2024-10-04 21:39:18 +0300

    feat: Enable buy/sell orders in strategy

--- strategy.py
+++ strategy.py
@@ -1,3 +1,35 @@
+from order import Order
+
 class Strategy():
+
     def __init__(self):
+        self.current_idx = None
+        self.data = None
+        self.orders = []
+        self.trades = []
+
+    def buy(self, ticker, size=1):
+        self.orders.append(
+            Order(
+                ticker = ticker,
+                side = 'buy',
+                size = size,
+                idx = self.current_idx
+            ))
+
+    def sell(self, ticker, size=1):
+        self.orders.append(
+            Order(
+                ticker = ticker,
+                side = 'sell',
+                size = -size,
+                idx = self.current_idx
+            ))
+
+    @property
+    def position_size(self):
+        return sum([trade.size for trade in self.trades])
+
+    # creators will override/implement
+    def on_bar(self):
         pass
