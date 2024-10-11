a2e0feab2b0a613591e581e7c3a8cab50b459ef2
Drew Sommer <drew@sommerengineering.com>
2024-10-11 22:41:41 +0300

    feat: Fill long and sell (flatten) orders

--- engine.py
+++ engine.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from tqdm import tqdm
 
+from trade import Trade
+
+
 class Engine():
 
     # initialize
@@ -25,9 +28,35 @@ class Engine():
         for idx in tqdm(self.data.index):
             self.current_idx = idx
             self.strategy.current_idx = self.current_idx
-            self._fill_orders()
+            self.fill_orders()
             # self.strategy.on_bar()
             # print(idx)
 
-    def _fill_orders(self):
-        pass
+    def fill_orders(self):
+
+        for order in self.strategy.orders:
+
+            can_fill = False
+
+            # long: ensure enough cash exists to purchase qty
+            if order.side == 'buy' and self.cash >= self.data.loc[self.current_idx]['Open'] * order.size:
+                can_fill = True
+
+            # flatten (short): ensure position size is larger than qty sell
+            elif order.side == 'sell' and self.strategy.position_size >= order.size:
+                can_fill = True
+
+            if can_fill:
+                trade = Trade(
+                    ticker = order.ticker,
+                    side = order.side,
+                    price = self.data.loc[self.current_idx]['Open'],
+                    size = order.size,
+                    type = order.type,
+                    idx = self.current_idx)
+
+                self.strategy.trades.append(trade)
+                self.cash -= trade.price * trade.size
+
+        # clear orders since they are processed
+        self.strategy.orders = []
