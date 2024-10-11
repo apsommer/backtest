ce30d2fd89ca81c6068ee48140b27dbd82452a4f
Drew Sommer <drew@sommerengineering.com>
2024-10-11 22:47:33 +0300

    test: Simple strategy

---
+++ SimpleStrategyTest.py
@@ -0,0 +1,14 @@
+from strategy import Strategy
+
+# inherit from Strategy base class
+class SimpleStrategyTest(Strategy):
+
+    # override/implement on_bar
+    def on_bar(self):
+
+        if self.position_size == 0:
+            self.buy('AAPL', 1)
+            print(self.current_idx,"buy")
+        else:
+            self.sell('AAPL', 1)
+            print(self.current_idx,"sell")
